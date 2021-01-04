3d0c06e6bb6b7a96ed69dadb4a43e3519e85684a
Francis Ricardo Ramirez <francisricardor@gmail.com>
2021-01-04 19:25:16 -0400

    Clase de cuenta corriente

---
+++ Clase/CuentaCorriente.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase
+{
+    /// <summary>
+    /// Clase para administrar las cuentas corrientes.
+    /// </summary>
+    public class CuentaCorriente
+    {
+
+        #region"Propiedades"
+        private string _numeroCuenta;
+
+        public string NumeroCuenta
+        {
+            get { return _numeroCuenta; }
+            set
+            {
+                _numeroCuenta = value;
+            }
+        }
+
+        private string _nombre;
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        private decimal _saldo;
+
+        public decimal Saldo
+        {
+            get { return _saldo; }
+            set { _saldo = value; }
+        }
+        #endregion
+
+        #region "Metodos"
+        public void Depositar()
+        {
+            Console.WriteLine("Estoy Depositando");
+        }
+        public void Girar()
+        {
+            Console.WriteLine("Estoy Girando");
+        }
+        public decimal ContultarSaldo()
+        {
+            return this._saldo;
+        }
+        #endregion
+
+    }
+}
--- Clase/Program.cs
+++ Clase/Program.cs
@@ -6,7 +6,24 @@ namespace Clase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CuentaCorriente cuentaCorriente = new CuentaCorriente();
+
+            cuentaCorriente.NumeroCuenta = "78945612";
+            cuentaCorriente.Nombre = "Mi cuenta cuenta corriente";
+            cuentaCorriente.Saldo = 500;
+
+            cuentaCorriente.Depositar();
+            cuentaCorriente.Girar();
+
+            var saldo = cuentaCorriente.ContultarSaldo();
+
+            Console.WriteLine($"Mi numero de cuenta {cuentaCorriente.NumeroCuenta}");
+            Console.WriteLine($"Nombre de la cuenta {cuentaCorriente.Nombre} ");
+            Console.WriteLine($"Saldo de la cuenta {cuentaCorriente.Saldo}");
+
+
+
+            Console.ReadLine();
         }
     }
 }
