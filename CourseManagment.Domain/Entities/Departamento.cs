2f1ab2f55909a3e4f0241715ab44bee45283ea76
Francis Ricardo Ramirez <francisricardor@gmail.com>
2021-01-25 20:04:42 -0400

    Agregando entidad de departamento.

--- CourseManagment.Domain/Entities/Departamento.cs
+++ CourseManagment.Domain/Entities/Departamento.cs
@@ -4,10 +4,11 @@ using System.Text;
 
 namespace CourseManagment.Domain.Entities
 {
-    public class Departamento : BaseEntity
+    public class Departamento 
     {
         public int DepartamentoId { get; set; }
         public string Nombre { get; set; }
+        public string Area { get; set; }
 
     }
 }
